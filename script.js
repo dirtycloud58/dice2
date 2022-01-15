4ec9d5c621364da1756ebbb4f2ba62e3eb9b6ffd
frederic <dewaele-frederic@outlook.fr>
2022-01-15 13:56:18 +0100

    final js et mise en forme css

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const player1 = document.getElementById("player1");
-const player2 = document.getElementById("player2");
+const player1 = document.querySelector(".player1");
+const player2 = document.querySelector(".player2");
 const scoreTotal1 = document.getElementById("scoreTotal1");
 const scoreTotal2 = document.getElementById("scoreTotal2");
 const score1 = document.getElementById("score1");
@@ -16,8 +16,8 @@ activePlayer = player1;
 function jeter() {
 
     let number = Math.floor(Math.random() * (6 - 1 +1) +1);
-    dice.textContent = number;
-    console.log(dice);
+    dice.innerHTML = "<img src=./IMG/dice/dice-" + number + ".png>";
+
 
     if(number == 1){
         if(activePlayer == player1){
@@ -29,9 +29,9 @@ function jeter() {
         }
     }else {
         if(activePlayer == player1){
-            score1.textContent = parseInt(score1.textContent) + number;
+            score1.textContent = parseInt(score1.textContent) + parseInt(number);
     } else{
-            score2.textContent = parseInt(score2.textContent) + number;
+            score2.textContent = parseInt(score2.textContent) + parseInt(number);
     }
 }};
 
@@ -43,17 +43,45 @@ activePlayer = activePlayer == player1 ? player2 : player1;
 score1.textContent = 0;
 score2.textContent = 0;
 
+player1.classList.toggle("active");
+player2.classList.toggle("active");
+
 };
 
 function credit () {
     if(activePlayer == player1){
-        scoreTotal1.textContent = parseInt(score1.textContent);
-        nextPlayer();
+
+            scoreTotal1.textContent = parseInt(scoreTotal1.textContent) + parseInt(score1.textContent);
+            scoreTotal1.textContent >= 100 ? winGame() : nextPlayer();
+
+        } else{
+            scoreTotal2.textContent = parseInt(scoreTotal2.textContent) + parseInt(score2.textContent);
+            scoreTotal2.textContent >= 100 ? winGame() : nextPlayer();
+        }};
+
+
+function winGame() {
+
+    if(scoreTotal1 >= 100){
+        alert ('le joueur 1 remporte la partie !!');
     }else {
-        scoreTotal2.textContent = parseInt(score2.textContent);
-        nextPlayer();
+        alert ('le joueur 2 remporte la partie !!');
     }
-}
+    stop();
+};
+
+function stop() {
+
+    dice.innerHTML = "<img src=./IMG/dice/dice-" + number + ".png>";
+    score1.textContent = 0;
+    score2.textContent = 0;
+    scoreTotal1.textContent = 0;
+    scoreTotal2.textContent = 0;
+    player1.classList.add("active");
+    player2.classList.remove("active");
+    
+};
 
 lancer.addEventListener("click", jeter);
 save.addEventListener("click", credit);
+reset.addEventListener("click", stop);
